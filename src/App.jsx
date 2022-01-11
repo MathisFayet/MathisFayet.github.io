9a4a72ab0b0667891822913d7c882787760c1d8b
FAYET Mathis <60433005+MathisFayet@users.noreply.github.com>
2022-01-11 18:32:33 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="bg-blue-300">
+      <h1 className="flex justify-center "> new project</h1>
+    </div>
+  );
+}
+export default App;
